abd47141acb32d9d0147341e8b33e9dcaa205723
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-25 15:14:52 -0500

    Update File-9-2.py

--- File-9-2.py
+++ File-9-2.py
@@ -1,10 +1,15 @@
+#Do we have sympy?
+try:
+  from sympy import *
+except ImportError:
+  pass
 from math import pi,sin,cos,tan
 #Some py scripts I used for 9-2, area and perimeter of regular polygons
 #a - Apothem
 #p - perimeter
 #n - number of sides
 #r - polygonal radius
-#s - side lengths
+#s - side length
 def nas(n,a,s):
   return n*a*s/2
 def pa(p,a):
