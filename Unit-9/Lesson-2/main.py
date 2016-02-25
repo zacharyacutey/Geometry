b305605bc57ae597d301092aa84c2a79ba3b2ff2
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-25 14:44:03 -0500

    Update main.py

--- Unit-9/Lesson-2/main.py
+++ Unit-9/Lesson-2/main.py
@@ -1,3 +1,12 @@
 from math import pi,sin,cos,tan
 def nas(n,a,s):
-  
+  return n*a*s/2
+def pa(p,a):
+  return p*a/2
+#Little nicities that Harness failed to teach us
+def na(n,a):
+  return n*a**2*tan(pi/n)
+def ns(n,s):
+  return n*s**2/4/tan(pi/n)
+def nr(n,r):
+  return n*r**2/2*sin(2*pi/n)
