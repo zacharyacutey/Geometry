6e531376d4cdc152e1c38ffc79a1024f8a6028dc
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-29 15:19:04 -0500

    Update Lesson9-3.py

--- Lesson9-3.py
+++ Lesson9-3.py
@@ -4,7 +4,7 @@ from sympy import *
 #Diameter - d
 #Area - a
 def r_c(r):
-  return 2*r*pi
+  return r_d(r)*pi
 def r_a(r):
   return pi*r**2
 def r_d(r):
@@ -14,16 +14,16 @@ def d_r(d):
 def d_c(d):
   return d*pi
 def d_a(d):
-  return r_a(d/2)
+  return r_a(r_d(r))
 def c_a(c):
-  return d_a(c/pi)
+  return d_a(c_d(c))
 def c_d(c):
   return c/pi
 def c_r(c):
-  return c_d(c)/2
+  return d_r(c_d(c))
 def a_c(a):
-  return r_c(sqrt(a/pi))
+  return r_c(a_r(a))
 def a_r(a):
   return sqrt(a/pi)
 def a_d(a):
-  return 2*a_r(a)
+  return r_d(a_r(a))
