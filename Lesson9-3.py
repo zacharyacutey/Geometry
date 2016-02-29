526ae53b9ec2e15ba17beb85b6411fc7be46b595
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-29 15:16:16 -0500

    Update Lesson9-3.py

--- Lesson9-3.py
+++ Lesson9-3.py
@@ -5,13 +5,25 @@ from sympy import *
 #Area - a
 def r_c(r):
   return 2*r*pi
-def d_c(d):
-  return d*pi
 def r_a(r):
   return pi*r**2
+def r_d(r):
+  return 2*r
+def d_r(d):
+  return d/2
+def d_c(d):
+  return d*pi
 def d_a(d):
   return r_a(d/2)
 def c_a(c):
   return d_a(c/pi)
+def c_d(c):
+  return c/pi
+def c_r(c):
+  return c_d(c)/2
 def a_c(a):
   return r_c(sqrt(a/pi))
+def a_r(a):
+  return sqrt(a/pi)
+def a_d(a):
+  return 2*a_r(a)
